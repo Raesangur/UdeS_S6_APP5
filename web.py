b6a53332fb77fe3620134a3f89a3a9fc6f28b1f0
philoup26 <teamtetras@gmail.com>
2023-07-07 11:36:46 -0400

    nouveau serveur

--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 # Python 3 server example
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import simplejson
 import time
 
 hostName = "localhost"
@@ -15,6 +16,24 @@ class MyServer(BaseHTTPRequestHandler):
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
+    def do_Post(self):
+        self._set_headers()
+        print "in post method"
+        self.data_string = self.rfile.read(int(self.headers['Content-Length']))
+
+        self.send_response(200)
+        self.end_headers()
+
+        data = simplejson.loads(self.data_string)
+        with open("test123456.json", "w") as outfile:
+            simplejson.dump(data, outfile)
+        print "{}".format(data)
+        f = open("for_presen.py")
+        self.wfile.write(f.read())
+        return
+        
+
+
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
